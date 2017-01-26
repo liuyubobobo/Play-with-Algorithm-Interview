63ad8fb17c9562ed8bea65c2099b273b993ead6c
liuyubobobo <liuyubobobo@gmail.com>
2017-01-26 17:13:45 +0800

    Chapter 04 code and keynote first version completed.

---
+++ 04-Using-Hash-Table/Course Code (C++)/07-Contains-Duplicate-III/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <cassert>
+#include <stdexcept>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+
+        if( nums.size() <= 1 )
+            return false;
+
+        if( k <= 0 )
+            return false;
+
+        set<long long> record;
+        for( int i = 0 ; i < nums.size() ; i ++ ){
+
+            if( record.size() >= k + 1 )
+                record.erase( nums[i-k-1] );
+
+            if( record.lower_bound( (long long)nums[i] - (long long)t ) != record.end() &&
+                fabs(*record.lower_bound( (long long)nums[i] - (long long)t ) - (long long)nums[i]) <= t)
+                return true;
+
+            record.insert( nums[i] );
+        }
+
+        return false;
+    }
+};
+
+int main() {
+
+    int nums[] = {-2147483648,-2147483647};
+    vector<int> vec( nums, nums + sizeof(nums)/sizeof(int));
+    int k = 3;
+    int t = 3;
+
+    if( Solution().containsNearbyAlmostDuplicate(vec, k, t) )
+        cout<<"true"<<endl;
+    else
+        cout<<"false"<<endl;
+
+    return 0;
+}
