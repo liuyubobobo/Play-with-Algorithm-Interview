a6ac67e2cf6a88d7aa7155fff410fd534d7fa6d2
liuyubobobo <liuyubobobo@gmail.com>
2017-02-10 11:40:39 +0800

    Chahapter 03 project structure updated.

---
+++ 03-Using-Array/Course Code (C++)/04-Move-Zeroes-II/main4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    // 时间复杂度 O(n)
+    // 空间复杂度 O(1)
+    void moveZeroes(vector<int>& nums) {
+
+        int k = 0; // nums中, [0...k)的元素均为非0元素
+
+        for(int i = 0 ; i < nums.size() ; i ++ )
+            // 遍历到第i个元素后,保证[0...i]中所有非0元素
+            // 都按照顺序排列在[0...k)中
+            // 同时, [k...i] 为0
+            if( nums[i] ){
+                if( k != i )
+                    swap( nums[k] , nums[i] );
+                k ++;
+            }
+
+    }
+};
+
+int main() {
+
+    int arr[] = {0, 1, 0, 3, 12};
+    vector<int> vec(arr, arr + sizeof(arr)/sizeof(int));
+
+    Solution().moveZeroes(vec);
+
+    for( int i = 0 ; i < vec.size() ; i ++ )
+        cout<<vec[i]<<" ";
+    cout<<endl;
+
+    return 0;
+}
