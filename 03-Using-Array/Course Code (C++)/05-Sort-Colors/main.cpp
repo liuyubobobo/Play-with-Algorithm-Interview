c84883ea0131d4a7537e1c08938a0c73313dcdee
liuyubobobo <liuyubobobo@gmail.com>
2017-02-11 19:57:20 +0800

    Chapter 03 section 05 completed.

--- 03-Using-Array/Course Code (C++)/05-Sort-Colors/main.cpp
+++ 03-Using-Array/Course Code (C++)/05-Sort-Colors/main.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 using namespace std;
 
 // 时间复杂度: O(n)
 // 空间复杂度: O(k), k为元素的取值范围
+// 对整个数组遍历了两遍
 class Solution {
 public:
-    void sortColors(vector<int> &A) {
+    void sortColors(vector<int> &nums) {
 
-        int count[3] = {0, 0, 0};    // 存放0,1,2三个元素的频率
-        for( int i = 0 ; i < A.size() ; i ++ )
-            count[A[i]] ++;
+        int count[3] = {0};    // 存放0,1,2三个元素的频率
+        for( int i = 0 ; i < nums.size() ; i ++ ){
+            assert( nums[i] >= 0 && nums[i] <= 2 );
+            count[nums[i]] ++;
+        }
 
-        for( int i = 1 ; i < 3 ; i ++ )
-            count[i] += count[i-1];
-
+        int index = 0;
         for( int i = 0 ; i < count[0] ; i ++ )
-            A[i] = 0;
-        for( int i = count[0] ; i < count[1] ; i ++ )
-            A[i] = 1;
-        for( int i = count[1] ; i < count[2] ; i ++ )
-            A[i] = 2;
+            nums[index++] = 0;
+        for( int i = 0 ; i < count[1] ; i ++ )
+            nums[index++] = 1;
+        for( int i = 0 ; i < count[2] ; i ++ )
+            nums[index++] = 2;
 
         // 小练习: 更加自使用的计数排序
     }
--- 03-Using-Array/Course Code (C++)/05-Sort-Colors/main2.cpp
+++ 03-Using-Array/Course Code (C++)/05-Sort-Colors/main2.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 using namespace std;
 
 
 // 时间复杂度: O(n)
 // 空间复杂度: O(1)
+// 对整个数组只遍历了一遍
 class Solution {
 public:
-    void sortColors(vector<int> &A) {
+    void sortColors(vector<int> &nums) {
 
-        int zero = -1;      // [0...zero] == 0
-        int two = A.size(); // [two...n-1] == 0
-        for (int i = 0; i < two; ) {
-            if( A[i] == 2 )
-                swap( A[i] , A[--two] );
-            else if( A[i] == 0 )
-                swap( A[i++] , A[++zero] );
-            else
+        int zero = -1;          // [0...zero] == 0
+        int two = nums.size();  // [two...n-1] == 2
+        for( int i = 0 ; i < two ; ){
+            if( nums[i] == 1 )
                 i ++;
+            else if ( nums[i] == 2 )
+                swap( nums[i] , nums[--two]);
+            else{ // nums[i] == 0
+                assert( nums[i] == 0 );
+                swap( nums[++zero] , nums[i++] );
+            }
         }
     }
 };
