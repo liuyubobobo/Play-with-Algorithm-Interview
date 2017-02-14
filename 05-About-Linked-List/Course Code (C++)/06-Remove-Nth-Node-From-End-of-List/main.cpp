3da46790dfec4b3796a460529b62c2715c4bde42
liuyubobobo <liuyubobobo@gmail.com>
2017-02-15 03:39:51 +0800

    Chapter 05 section 06 added.

---
+++ 05-About-Linked-List/Course Code (C++)/06-Remove-Nth-Node-From-End-of-List/main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+///Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+/// LinkedList Test Helper Functions
+ListNode* createLinkedList(int arr[], int n){
+
+    if( n == 0 )
+        return NULL;
+
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* curNode = head;
+    for( int i = 1 ; i < n ; i ++ ){
+        curNode->next = new ListNode(arr[i]);
+        curNode = curNode->next;
+    }
+
+    return head;
+}
+
+void printLinkedList(ListNode* head){
+
+    if( head == NULL ){
+        cout<<"NULL"<<endl;
+        return;
+    }
+
+    ListNode* curNode = head;
+    while( curNode != NULL ){
+        cout<<curNode->val;
+        if( curNode->next != NULL )
+            cout<<" -> ";
+        curNode = curNode->next;
+    }
+
+    cout<<endl;
+
+    return;
+}
+
+// 先记录链表总长度
+// 需要对链表进行两边遍历
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+
+        int length = 0;
+        for(ListNode* cur = dummyHead->next ; cur != NULL ; cur = cur->next )
+            length ++;
+        //cout<<length<<endl;
+
+        int k = length - n;
+        assert( k >= 0 );
+        ListNode* cur = dummyHead;
+        for( int i = 0 ; i < k ; i ++ )
+            cur = cur -> next;
+
+        ListNode* delNode = cur->next;
+        cur->next = delNode->next;
+        delete delNode;
+
+        ListNode* retNode = dummyHead->next;
+        delete dummyHead;
+        return retNode;
+    }
+};
+
+int main() {
+
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(int);
+
+    ListNode* head = createLinkedList(arr, n);
+    printLinkedList(head);
+
+    head = Solution().removeNthFromEnd(head, 2);
+    printLinkedList(head);
+
+    return 0;
+}
---
+++ 05-About-Linked-List/Course Code (C++)/06-Remove-Nth-Node-From-End-of-List/main2.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+///Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+/// LinkedList Test Helper Functions
+ListNode* createLinkedList(int arr[], int n){
+
+    if( n == 0 )
+        return NULL;
+
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* curNode = head;
+    for( int i = 1 ; i < n ; i ++ ){
+        curNode->next = new ListNode(arr[i]);
+        curNode = curNode->next;
+    }
+
+    return head;
+}
+
+void printLinkedList(ListNode* head){
+
+    if( head == NULL ){
+        cout<<"NULL"<<endl;
+        return;
+    }
+
+    ListNode* curNode = head;
+    while( curNode != NULL ){
+        cout<<curNode->val;
+        if( curNode->next != NULL )
+            cout<<" -> ";
+        curNode = curNode->next;
+    }
+
+    cout<<endl;
+
+    return;
+}
+
+// 使用双指针, 对链表只遍历了一遍
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+
+        ListNode* p = dummyHead;
+        ListNode* q = dummyHead;
+        for( int i = 0 ; i < n + 1 ; i ++ )
+            q = q->next;
+
+        while( q ){
+            p = p->next;
+            q = q->next;
+        }
+
+        ListNode* delNode = p->next;
+        p->next = delNode->next;
+        delete delNode;
+
+        ListNode* retNode = dummyHead->next;
+        delete dummyHead;
+        return retNode;
+    }
+};
+
+int main() {
+
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(int);
+
+    ListNode* head = createLinkedList(arr, n);
+    printLinkedList(head);
+
+    head = Solution().removeNthFromEnd(head, 2);
+    printLinkedList(head);
+
+    return 0;
+}
