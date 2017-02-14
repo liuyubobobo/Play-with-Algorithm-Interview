313989213abe949278665dcf51de8611799a190f
liuyubobobo <liuyubobobo@gmail.com>
2017-02-15 02:48:11 +0800

    Chapter 05 section 05 code added.

---
+++ 05-About-Linked-List/Course Code (C++)/05-Delete-Node-in-a-Linked-List/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+
+/// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+
+        if( node == NULL )
+            return;
+
+        if( node->next == NULL ){
+            delete node;
+            node = NULL;
+            return;
+        }
+
+        node->val = node->next->val;
+        ListNode* delNode = node->next;
+        node->next = delNode->next;
+        delete delNode;
+
+        return;
+    }
+};
+
+int main() {
+
+    return 0;
+}
