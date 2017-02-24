2d29e6b4126a023948850eded34eef210ca525b8
liuyubobobo <liuyubobobo@gmail.com>
2017-02-25 00:41:06 +0800

    Chapter 07 section 02 code completed.

---
+++ 07-Binary-Tree-and-Recursion/Course Code (C++)/02-1-Path-Sum/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+/// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+
+        if( root == NULL )
+            return false;
+
+        if( root->left == NULL && root->right == NULL )
+            return sum == root->val;
+
+        return hasPathSum( root->left , sum - root->val )
+                || hasPathSum( root->right , sum - root->val );
+    }
+};
+
+int main() {
+
+    return 0;
+}
---
+++ 07-Binary-Tree-and-Recursion/Course Code (C++)/02-2-Path-Sum-III/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+/// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+
+public:
+    int pathSum(TreeNode* root, int sum) {
+
+        if( root == NULL )
+            return 0;
+
+        return findPath( root , sum )
+                + pathSum( root->left , sum )
+                + pathSum( root->right , sum );
+    }
+
+private:
+
+    int findPath( TreeNode* node, int num){
+
+        if( node == NULL )
+            return 0;
+
+        int res = 0;
+        if( node->val == num )
+            res += 1;
+
+        res += findPath( node->left , num - node->val );
+        res += findPath( node->right , num - node->val );
+
+        return res;
+    }
+};
+
+int main() {
+
+    TreeNode* node1 = new TreeNode(3);
+    TreeNode* node2 = new TreeNode(-2);
+
+    TreeNode* node3 = new TreeNode(3);
+    node3->left = node1;
+    node3->right = node2;
+
+    TreeNode* node4 = new TreeNode(1);
+    TreeNode* node5 = new TreeNode(2);
+    node5->right = node4;
+
+    TreeNode* node6 = new TreeNode(5);
+    node6->left = node3;
+    node6->right = node5;
+
+    TreeNode* node7 = new TreeNode(11);
+    TreeNode* node8 = new TreeNode(-3);
+    node8->right = node7;
+
+    TreeNode* node9 = new TreeNode(10);
+    node9->left = node6;
+    node9->right = node8;
+
+    cout<<Solution().pathSum( node9, 8)<<endl;
+
+    return 0;
+}
