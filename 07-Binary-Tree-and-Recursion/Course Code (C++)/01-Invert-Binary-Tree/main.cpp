1cdfbabd2f675972147064fbb6999a38b2b45931
liuyubobobo <liuyubobobo@gmail.com>
2017-02-24 17:11:32 +0800

    Chapter 07 section 01 code completed.

---
+++ 07-Binary-Tree-and-Recursion/Course Code (C++)/01-Invert-Binary-Tree/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+/// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if( root == NULL)
+            return NULL;
+        swap( root->left, root->right);
+        invertTree( root->left );
+        invertTree( root->right );
+        return root;
+    }
+};
+
+int main() {
+
+    return 0;
+}
