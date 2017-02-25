e07212a896c2f8a79b55f65d81986640623a8837
liuyubobobo <liuyubobobo@gmail.com>
2017-02-25 13:19:36 +0800

    Chapter 07 section 01 restructured.

---
+++ 07-Binary-Tree-and-Recursion/Course Code (C++)/01-Maximum-Depth-of-Binary-Tree/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+/// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+
+        if( root == NULL )
+            return 0;
+
+        return 1 + max( maxDepth(root->left) , maxDepth(root->right) );
+    }
+};
+
+int main() {
+
+    return 0;
+}
