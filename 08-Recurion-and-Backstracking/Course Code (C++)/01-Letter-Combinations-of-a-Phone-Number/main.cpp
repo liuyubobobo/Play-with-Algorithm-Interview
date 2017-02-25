e59b51d937b389adaa26de5f3a1d3c50782149b1
liuyubobobo <liuyubobobo@gmail.com>
2017-02-26 01:02:44 +0800

    Chapter 08 section 01 code completed.

---
+++ 08-Recurion-and-Backstracking/Course Code (C++)/01-Letter-Combinations-of-a-Phone-Number/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+private:
+    const string letterMap[10] = {
+            " ",    //0
+            "",     //1
+            "abc",  //2
+            "def",  //3
+            "ghi",  //4
+            "jkl",  //5
+            "mno",  //6
+            "pqrs", //7
+            "tuv",  //8
+            "wxyz"  //9
+    };
+
+    vector<string> res;
+
+    void findCombination(const string &digits, int index, const string &s){
+
+        if( index == digits.size() ){
+            res.push_back( s );
+            return;
+        }
+
+        char c = digits[index];
+        string letters = letterMap[c-'0'];
+        for( int i = 0 ; i < letters.size() ; i ++ )
+            findCombination(digits, index+1, s + letters[i]);
+
+        return;
+    }
+
+public:
+    vector<string> letterCombinations(string digits) {
+
+        if( digits == "" )
+            return res;
+
+        findCombination(digits, 0, "");
+
+        return res;
+    }
+};
+
+int main() {
+
+    vector<string> res = Solution().letterCombinations("23");
+    for( int i = 0 ; i < res.size() ; i ++ )
+        cout<<res[i]<<endl;
+
+    return 0;
+}
