bfb2c7a340bbd8b5c0ee13ae94f487404690c2bf
liuyubobobo <liuyubobobo@gmail.com>
2017-03-01 01:46:24 +0800

    Chapter 08 section 01-02 completed.

--- 08-Recurion-and-Backstracking/Course Code (C++)/01-Letter-Combinations-of-a-Phone-Number/main.cpp
+++ 08-Recurion-and-Backstracking/Course Code (C++)/01-Letter-Combinations-of-a-Phone-Number/main.cpp
@@ -22,18 +22,26 @@ private:
 
     vector<string> res;
 
+    // s中保存了此时从digits[0...index-1]翻译得到的一个字母字符串
+    // 寻找和digits[index]匹配的字母, 获得digits[0...index]翻译得到的解
     void findCombination(const string &digits, int index, const string &s){
 
+        cout<<index<<" : "<<s<<endl;
         if( index == digits.size() ){
             res.push_back( s );
+            cout<<"get "<<s<<" , return"<<endl;
             return;
         }
 
         char c = digits[index];
-        assert( c >= '0' && c <= '9' );
+        assert( c >= '0' && c <= '9' && c != '1');
         string letters = letterMap[c-'0'];
-        for( int i = 0 ; i < letters.size() ; i ++ )
+        for( int i = 0 ; i < letters.size() ; i ++ ){
+            cout<<"digits["<<index<<"] = "<<c<<" , use "<<letters[i]<<endl;
             findCombination(digits, index+1, s + letters[i]);
+        }
+
+        cout<<"digits["<<index<<"] = "<<c<<" complete, return"<<endl;
 
         return;
     }
@@ -54,7 +62,7 @@ public:
 
 int main() {
 
-    vector<string> res = Solution().letterCombinations("23");
+    vector<string> res = Solution().letterCombinations("234");
     for( int i = 0 ; i < res.size() ; i ++ )
         cout<<res[i]<<endl;
 
