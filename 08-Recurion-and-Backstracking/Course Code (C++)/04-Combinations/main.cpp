6724a3ebb816f6b4679a3ab1cab2820f3d9caf87
liuyubobobo <liuyubobobo@gmail.com>
2017-03-01 14:18:11 +0800

    Chapter 08 section 04-05 code completed.

---
+++ 08-Recurion-and-Backstracking/Course Code (C++)/04-Combinations/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+private:
+    vector<vector<int>> res;
+
+    // 求解C(n,k), 当前已经找到的组合存储在c中, 需要从start开始搜索新的元素
+    void generateCombinations(int n, int k, int start, vector<int> &c){
+
+        if( c.size() == k ){
+            res.push_back(c);
+            return;
+        }
+
+        for( int i = start ; i <= n ; i ++ ){
+            c.push_back( i );
+            generateCombinations(n, k, i + 1 , c );
+            c.pop_back();
+        }
+
+        return;
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+
+        res.clear();
+        if( n <= 0 || k <= 0 )
+            return res;
+
+        vector<int> c;
+        generateCombinations(n, k, 1, c);
+
+        return res;
+    }
+};
+
+int main() {
+
+    vector<vector<int>> res = Solution().combine(4,2);
+    for( int i = 0 ; i < res.size() ; i ++ ){
+        for( int j = 0 ; j < res[i].size() ; j ++ )
+            cout<<res[i][j]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
---
+++ 08-Recurion-and-Backstracking/Course Code (C++)/05-Word-Search/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
+class Solution {
+private:
+    int d[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
+    int m,n;
+    vector<vector<bool>> visited;
+
+    bool inArea( int x , int y ){
+        return x >= 0 && x < m && y >= 0 && y < n;
+    }
+
+    // 在board[startx][starty]寻找word[index]
+    bool searchWord( const vector<vector<char>> &board, const string& word, int index,
+                    int startx, int starty ){
+
+        assert( inArea(startx,starty) );
+        if( index == word.size() - 1 )
+            return board[startx][starty] == word[index];
+
+        if( board[startx][starty] == word[index] ){
+            visited[startx][starty] = true;
+            for( int i = 0 ; i < 4 ; i ++ ){
+                int newx = startx + d[i][0];
+                int newy = starty + d[i][1];
+                if( inArea(newx, newy) && !visited[newx][newy] &&
+                    searchWord( board , word , index + 1 , newx , newy ) )
+                    return true;
+            }
+            visited[startx][starty] = false;
+        }
+        return false;
+    }
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+
+        m = board.size();
+        assert( m > 0 );
+        n = board[0].size();
+        visited = vector<vector<bool> >(m,vector<bool>(n,false));
+        for( int i = 0 ; i < board.size() ; i ++ )
+            for( int j = 0 ; j < board[i].size() ; j ++ )
+                if( searchWord( board, word, 0 , i, j) )
+                    return true;
+
+        return false;
+    }
+};
+
+int main() {
+
+    char b[3][4] = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+    vector<vector<char>> board;
+    for( int i = 0 ; i < 3 ; i ++ )
+        board.push_back( vector<char>(b[i],b[i]+sizeof(b[i])/sizeof(char)) );
+
+    int cases = 3;
+    string words[3] = {"ABCCED" , "SEE" , "ABCB" };
+    for( int i = 0 ; i < cases ; i ++ )
+        if( Solution().exist(board,words[i]))
+            cout<<"found "<<words[i]<<endl;
+        else
+            cout<<"can not found "<<words[i]<<endl;
+
+    return 0;
+}
