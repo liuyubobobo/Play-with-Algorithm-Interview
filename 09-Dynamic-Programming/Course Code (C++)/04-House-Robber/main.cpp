a7c1886ccd3b89feed7793cd592a7d2a6883b166
liuyubobobo <liuyubobobo@gmail.com>
2017-03-05 00:39:42 +0800

    Chapter 09 section 04 code completed.

---
+++ 09-Dynamic-Programming/Course Code (C++)/04-House-Robber/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+private:
+    vector<int> memo;
+
+    int tryRob( vector<int> &nums, int index){
+
+        if( index >= nums.size() )
+            return 0;
+
+        if( memo[index] != -1 )
+            return memo[index];
+
+        int res = 0;
+        for( int i = index ; i < nums.size() ; i ++ )
+            res = max(res, nums[i] + tryRob(nums, i+2));
+        memo[index] = res;
+        return res;
+    }
+public:
+    int rob(vector<int>& nums) {
+
+        memo = vector<int>(nums.size(), -1);
+        return tryRob(nums, 0);
+    }
+};
+
+int main() {
+
+    return 0;
+}
---
+++ 09-Dynamic-Programming/Course Code (C++)/04-House-Robber/main2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+
+public:
+    int rob(vector<int>& nums) {
+
+        int n = nums.size();
+        if( n == 0 )
+            return 0;
+
+        vector<int> memo(n, 0);
+        memo[n-1] = nums[n-1];
+        for( int i = n-2 ; i >= 0 ; i -- ) {
+            memo[i] = memo[i+1];
+            for (int j = i; j < n; j++)
+                memo[i] = max(memo[i], nums[j] + (j + 2 < n ? memo[j + 2] : 0) );
+        }
+        return memo[0];
+    }
+};
+
+int main() {
+
+    int nums[] = {2,1};
+    vector<int> vec(nums, nums+sizeof(nums)/sizeof(int));
+
+    cout<<Solution().rob(vec)<<endl;
+
+    return 0;
+}
