e8b490f4a02b5e1b14b2c12f3a1c7bb2fd21ca22
liuyubobobo <liuyubobobo@gmail.com>
2017-09-06 16:15:32 -0700

    Chapter 05 section 05 bug fixed.

--- 05-About-Linked-List/Course Code (C++)/05-Delete-Node-in-a-Linked-List/main.cpp
+++ 05-About-Linked-List/Course Code (C++)/05-Delete-Node-in-a-Linked-List/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
@@ -10,18 +11,64 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
+
+/// LinkedList Test Helper Functions
+ListNode* createLinkedList(int arr[], int n){
+
+    if( n == 0 )
+        return NULL;
+
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* curNode = head;
+    for( int i = 1 ; i < n ; i ++ ){
+        curNode->next = new ListNode(arr[i]);
+        curNode = curNode->next;
+    }
+
+    return head;
+}
+
+void printLinkedList(ListNode* head){
+
+    ListNode* curNode = head;
+    while( curNode != NULL ){
+        cout << curNode->val << " -> ";
+        curNode = curNode->next;
+    }
+
+    cout<<"NULL"<<endl;
+
+    return;
+}
+
+void deleteLinkedList(ListNode* head){
+
+    ListNode* curNode = head;
+    while( curNode != NULL ){
+        ListNode* delNode = curNode;
+        curNode = curNode->next;
+        delete delNode;
+    }
+
+    return;
+}
+
+ListNode* findNode(ListNode* head, int x){
+
+    ListNode* curNode = head;
+    while(curNode != NULL){
+        if(curNode->val == x)
+            return curNode;
+        curNode = curNode->next;
+    }
+    return NULL;
+}
+
 class Solution {
 public:
     void deleteNode(ListNode* node) {
 
-        if( node == NULL )
-            return;
-
-        if( node->next == NULL ){
-            delete node;
-            node = NULL;
-            return;
-        }
+        assert(node != NULL && node->next != NULL);
 
         node->val = node->next->val;
         ListNode* delNode = node->next;
@@ -34,6 +81,19 @@ public:
 };
 
 int main() {
+
+    int arr[] = {1, 2, 3, 4};
+    int n = sizeof(arr)/sizeof(int);
+
+    ListNode* head = createLinkedList(arr, n);
+    printLinkedList(head);
+
+    // 找到值为2的节点, 并调用我们的算法删除
+    ListNode* node2 = findNode(head, 2);
+    Solution().deleteNode(node2);
+    printLinkedList(head);
+
+    deleteLinkedList(head);
 
     return 0;
 }
