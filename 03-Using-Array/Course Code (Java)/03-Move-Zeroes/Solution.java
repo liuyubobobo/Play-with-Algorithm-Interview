45eba964b0335f6be286cbfc1f2f8b5f35051c47
liuyubobobo <liuyubobobo@gmail.com>
2017-10-06 17:58:14 -0700

    Chapter 03 section 03 Java code added.

--- 03-Using-Array/Course Code (Java)/03-Move-Zeroes/Solution.java
+++ 03-Using-Array/Course Code (Java)/03-Move-Zeroes/Solution.java
@@ -1,20 +1,16 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
+import java.util.*;
 
 // 时间复杂度 O(n)
 // 空间复杂度 O(n)
 class Solution {
-public:
-    void moveZeroes(vector<int>& nums) {
+    public void moveZeroes(int[] nums) {
 
-        vector<int> nonZeroElements;
+        ArrayList<Integer> nonZeroElements = new ArrayList<Integer>();
 
         // 将vec中所有非0元素放入nonZeroElements中
         for( int i = 0 ; i < nums.size() ; i ++ )
             if( nums[i] )
-                nonZeroElements.push_back( nums[i] );
+                nonZeroElements.add( nums[i] );
 
         // 将nonZeroElements中的所有元素依次放入到nums开始的位置
         for( int i = 0 ; i < nonZeroElements.size() ; i ++ )
@@ -24,18 +20,4 @@ public:
         for( int i = nonZeroElements.size() ; i < nums.size() ; i ++ )
             nums[i] = 0;
     }
-};
-
-int main() {
-
-    int arr[] = {0, 1, 0, 3, 12};
-    vector<int> vec(arr, arr + sizeof(arr)/sizeof(int));
-
-    Solution().moveZeroes(vec);
-
-    for( int i = 0 ; i < vec.size() ; i ++ )
-        cout<<vec[i]<<" ";
-    cout<<endl;
-
-    return 0;
 }
