bf3b225d6e8aeaca7e329236712ae3b96ce3841b
liuyubobobo <liuyubobobo@gmail.com>
2017-11-13 02:03:52 -0800

    Chapter 03 section 03 codes updated.

--- 03-Using-Array/Course Code (C++)/03-Move-Zeroes/main.cpp
+++ 03-Using-Array/Course Code (C++)/03-Move-Zeroes/main.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-// 时间复杂度 O(n)
-// 空间复杂度 O(n)
+// 时间复杂度: O(n)
+// 空间复杂度: O(n)
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
@@ -12,16 +12,16 @@ public:
         vector<int> nonZeroElements;
 
         // 将vec中所有非0元素放入nonZeroElements中
-        for( int i = 0 ; i < nums.size() ; i ++ )
-            if( nums[i] )
-                nonZeroElements.push_back( nums[i] );
+        for(int i = 0 ; i < nums.size() ; i ++)
+            if(nums[i])
+                nonZeroElements.push_back(nums[i]);
 
         // 将nonZeroElements中的所有元素依次放入到nums开始的位置
-        for( int i = 0 ; i < nonZeroElements.size() ; i ++ )
+        for(int i = 0 ; i < nonZeroElements.size() ; i ++)
             nums[i] = nonZeroElements[i];
 
         // 将nums剩余的位置放置为0
-        for( int i = nonZeroElements.size() ; i < nums.size() ; i ++ )
+        for(int i = nonZeroElements.size() ; i < nums.size() ; i ++)
             nums[i] = 0;
     }
 };
@@ -33,9 +33,9 @@ int main() {
 
     Solution().moveZeroes(vec);
 
-    for( int i = 0 ; i < vec.size() ; i ++ )
-        cout<<vec[i]<<" ";
-    cout<<endl;
+    for(int i = 0 ; i < vec.size() ; i ++)
+        cout << vec[i] << " ";
+    cout << endl;
 
     return 0;
 }
