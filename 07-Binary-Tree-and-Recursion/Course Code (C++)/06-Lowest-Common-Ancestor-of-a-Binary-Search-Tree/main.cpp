21f4aa5fbf083d263f56f2c055785714fcd536b5
liuyubobobo <liuyubobobo@gmail.com>
2017-11-18 00:49:45 -0800

    Chapter 07 section 06 C++ codes updated. Java codes added.

--- 07-Binary-Tree-and-Recursion/Course Code (C++)/06-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/main.cpp
+++ 07-Binary-Tree-and-Recursion/Course Code (C++)/06-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/main.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+/// 235. Lowest Common Ancestor of a Binary Search Tree
+/// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+/// 时间复杂度: O(lgn), 其中n为树的节点个数
+/// 空间复杂度: O(h), 其中h为树的高度
+
 /// Definition for a binary tree node.
 struct TreeNode {
     int val;
@@ -15,18 +20,18 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
 
-        assert( p != NULL && q != NULL );
+        assert(p != NULL && q != NULL);
 
-        if( root == NULL )
+        if(root == NULL)
             return NULL;
 
-        if( p->val < root->val && q->val < root->val )
-            return lowestCommonAncestor( root->left, p, q);
-        if( p->val > root->val && q->val > root->val )
-            return lowestCommonAncestor( root->right, p, q);
+        if(p->val < root->val && q->val < root->val)
+            return lowestCommonAncestor(root->left, p, q);
+        if(p->val > root->val && q->val > root->val)
+            return lowestCommonAncestor(root->right, p, q);
 
-        assert( p->val == root->val || q->val == root->val
-                || (root->val-p->val)*(root->val-q->val) < 0 );
+        assert(p->val == root->val || q->val == root->val
+                || (root->val - p->val) * (root->val - q->val) < 0);
 
         return root;
     }
---
+++ 07-Binary-Tree-and-Recursion/Course Code (Java)/06-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/src/Solution.java
@@ -0,0 +1,33 @@
+/// 235. Lowest Common Ancestor of a Binary Search Tree
+/// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+/// 时间复杂度: O(lgn), 其中n为树的节点个数
+/// 空间复杂度: O(h), 其中h为树的高度
+class Solution {
+
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if(p == null || q == null)
+            throw new IllegalArgumentException("p or q can not be null.");
+
+        if(root == null)
+            return null;
+
+        if(p.val < root.val && q.val < root.val)
+            return lowestCommonAncestor(root.left, p, q);
+        if(p.val > root.val && q.val > root.val)
+            return lowestCommonAncestor(root.right, p, q);
+
+        assert p.val == root.val || q.val == root.val
+                || (root.val - p.val) * (root.val - q.val) < 0;
+
+        return root;
+    }
+}
