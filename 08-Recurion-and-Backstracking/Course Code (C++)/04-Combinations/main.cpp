950f36d3530e037d501177ad9b647e5372ce72d3
liuyubobobo <liuyubobobo@gmail.com>
2017-11-18 15:53:02 -0800

    Chapter 08 section 04 C++ codes updated. Java codes added.

--- 08-Recurion-and-Backstracking/Course Code (C++)/04-Combinations/main.cpp
+++ 08-Recurion-and-Backstracking/Course Code (C++)/04-Combinations/main.cpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+/// 77. Combinations
+/// https://leetcode.com/problems/combinations/description/
+/// 时间复杂度: O(n^k)
+/// 空间复杂度: O(k)
 class Solution {
 private:
     vector<vector<int>> res;
@@ -10,14 +14,14 @@ private:
     // 求解C(n,k), 当前已经找到的组合存储在c中, 需要从start开始搜索新的元素
     void generateCombinations(int n, int k, int start, vector<int> &c){
 
-        if( c.size() == k ){
+        if(c.size() == k){
             res.push_back(c);
             return;
         }
 
-        for( int i = start ; i <= n ; i ++ ){
+        for(int i = start ; i <= n ; i ++){
             c.push_back( i );
-            generateCombinations(n, k, i + 1 , c );
+            generateCombinations(n, k, i + 1, c);
             c.pop_back();
         }
 
@@ -31,7 +35,7 @@ public:
             return res;
 
         vector<int> c;
-        generateCombinations(n, k, 1, c);
+        generateCombinations(n, k, 0, c);
 
         return res;
     }
