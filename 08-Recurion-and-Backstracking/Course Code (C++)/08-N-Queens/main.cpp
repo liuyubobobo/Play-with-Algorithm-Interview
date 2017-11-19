922dc27ef1748109ee8ea33bdc0b715c9a77a3ff
liuyubobobo <liuyubobobo@gmail.com>
2017-11-18 18:20:57 -0800

    Chapter 08 section 08 C++ codes updated. java codes added.

--- 08-Recurion-and-Backstracking/Course Code (C++)/08-N-Queens/main.cpp
+++ 08-Recurion-and-Backstracking/Course Code (C++)/08-N-Queens/main.cpp
@@ -4,53 +4,66 @@
 
 using namespace std;
 
+/// 51. N-Queens
+/// https://leetcode.com/problems/n-queens/description/
+/// 时间复杂度: O(n^n)
+/// 空间复杂度: O(n)
 class Solution {
 private:
     vector<bool> col, dia1, dia2;
     vector<vector<string>> res;
 
     // 尝试在一个n皇后问题中, 摆放第index行的皇后位置
-    void putQueen(int n , int index, vector<int> &row){
+    void putQueen(int n, int index, vector<int> &row){
 
-        if( index == n ){
-            res.push_back( generateBoard(n,row) );
+        if(index == n){
+            res.push_back(generateBoard(n, row));
             return;
         }
 
-        for( int i = 0 ; i < n ; i ++ )
+        for(int i = 0 ; i < n ; i ++)
             // 尝试将第index行的皇后摆放在第i列
-            if( !col[i] && !dia1[index+i] && !dia2[index-i+n-1] ){
+            if(!col[i] && !dia1[index + i] && !dia2[index - i + n - 1]){
                 row.push_back(i);
                 col[i] = true;
-                dia1[index+i] = true;
-                dia2[index-i+n-1] = true;
+                dia1[index + i] = true;
+                dia2[index - i + n - 1] = true;
                 putQueen(n, index + 1, row);
                 col[i] = false;
-                dia1[index+i] = false;
-                dia2[index-i+n-1] = false;
+                dia1[index + i] = false;
+                dia2[index - i + n - 1] = false;
                 row.pop_back();
             }
 
         return;
     }
 
-    vector<string> generateBoard( int n, vector<int> &row ){
-        assert( row.size() == n );
+    vector<string> generateBoard(int n, vector<int> &row){
+        assert(row.size() == n);
         vector<string> board(n, string(n, '.'));
-        for( int i = 0 ; i < n ; i ++ )
+        for(int i = 0 ; i < n ; i ++)
             board[i][row[i]] = 'Q';
         return board;
     }
+
 public:
     vector<vector<string>> solveNQueens(int n) {
 
         res.clear();
-        col = vector<bool>(n, false);
-        dia1 = vector<bool>(2*n-1, false);
-        dia2 = vector<bool>(2*n-1, false);
+
+        col.clear();
+        for(int i = 0 ; i < n ; i ++)
+            col.push_back(false);
+
+        dia1.clear();
+        dia2.clear();
+        for(int i = 0 ; i < 2 * n - 1 ; i ++){
+            dia1.push_back(false);
+            dia2.push_back(false);
+        }
 
         vector<int> row;
-        putQueen(n , 0 , row);
+        putQueen(n, 0, row);
 
         return res;
     }
@@ -60,10 +73,10 @@ int main() {
 
     int n = 4;
     vector<vector<string>> res = Solution().solveNQueens(n);
-    for( int i = 0 ; i < res.size() ; i ++ ){
-        for( int j = 0 ; j < n ; j ++)
-            cout<<res[i][j]<<endl;
-        cout<<endl;
+    for(int i = 0 ; i < res.size() ; i ++){
+        for(int j = 0 ; j < n ; j ++)
+            cout << res[i][j] << endl;
+        cout << endl;
     }
 
     return 0;
