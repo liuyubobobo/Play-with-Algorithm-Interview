f316e3650af5e0588a63c980de386e5cbeec3494
liuyubobobo <liuyubobobo@gmail.com>
2017-11-18 16:11:23 -0800

    Chapter 08 section 05 C++ codes updated. Java codes added.

--- 08-Recurion-and-Backstracking/Course Code (C++)/05-Combinations-optimized/main.cpp
+++ 08-Recurion-and-Backstracking/Course Code (C++)/05-Combinations-optimized/main.cpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+/// 77. Combinations
+/// https://leetcode.com/problems/combinations/description/
+/// 时间复杂度: O(n^k)
+/// 空间复杂度: O(k)
 class Solution {
 private:
     vector<vector<int>> res;
@@ -10,16 +14,16 @@ private:
     // 求解C(n,k), 当前已经找到的组合存储在c中, 需要从start开始搜索新的元素
     void generateCombinations(int n, int k, int start, vector<int> &c){
 
-        if( c.size() == k ){
+        if(c.size() == k){
             res.push_back(c);
             return;
         }
 
-        // 还有k - c.size()个空位, 所以,[i...n]中至少要有k-c.size()个元素
-        // i最多为 n - (k-c.size()) + 1
-        for( int i = start ; i <= n - (k-c.size()) + 1 ; i ++ ){
-            c.push_back( i );
-            generateCombinations(n, k, i + 1 , c );
+        // 还有k - c.size()个空位, 所以, [i...n] 中至少要有 k - c.size() 个元素
+        // i最多为 n - (k - c.size()) + 1
+        for(int i = start; i <= n - (k - c.size()) + 1 ; i ++){
+            c.push_back(i);
+            generateCombinations(n, k, i + 1 ,c);
             c.pop_back();
         }
 
@@ -29,7 +33,7 @@ public:
     vector<vector<int>> combine(int n, int k) {
 
         res.clear();
-        if( n <= 0 || k <= 0 || k > n )
+        if(n <= 0 || k <= 0 || k > n)
             return res;
 
         vector<int> c;
---
+++ 08-Recurion-and-Backstracking/Course Code (Java)/05-Combinations-optimized/src/Solution.java
@@ -0,0 +1,56 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/// 77. Combinations
+/// https://leetcode.com/problems/combinations/description/
+/// 时间复杂度: O(n^k)
+/// 空间复杂度: O(k)
+public class Solution {
+
+    private ArrayList<List<Integer>> res;
+
+    public List<List<Integer>> combine(int n, int k) {
+
+        res = new ArrayList<List<Integer>>();
+        if(n <= 0 || k <= 0 || k > n)
+            return res;
+
+        LinkedList<Integer> c = new LinkedList<Integer>();
+        generateCombinations(n, k, 1, c);
+
+        return res;
+    }
+
+    // 求解C(n,k), 当前已经找到的组合存储在c中, 需要从start开始搜索新的元素
+    private void generateCombinations(int n, int k, int start, LinkedList<Integer> c){
+
+        if(c.size() == k){
+            res.add((List<Integer>)c.clone());
+            return;
+        }
+
+        // 还有k - c.size()个空位, 所以, [i...n] 中至少要有 k - c.size() 个元素
+        // i最多为 n - (k - c.size()) + 1
+        for(int i = start ; i <= n - (k - c.size()) + 1 ; i ++){
+            c.addLast(i);
+            generateCombinations(n, k, i + 1, c);
+            c.removeLast();
+        }
+
+        return;
+    }
+
+    private static void printList(List<Integer> list){
+        for(Integer e: list)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        List<List<Integer>> res = (new Solution()).combine(4, 2);
+        for(List<Integer> list: res)
+            printList(list);
+    }
+}
