0bc4f93531d9c2f0a82c323a7ff6b73c285820cc
liuyubobobo <liuyubobobo@gmail.com>
2017-11-18 16:27:12 -0800

    Chapter 08 section 06 codes updated.

--- 08-Recurion-and-Backstracking/Course Code (C++)/06-Word-Search/main.cpp
+++ 08-Recurion-and-Backstracking/Course Code (C++)/06-Word-Search/main.cpp
@@ -1,16 +1,15 @@
-/// Source : https://leetcode.com/problems/word-search/description/
-/// Author : liuyubobobo
-/// Time   : 2017-05-05
-
 #include <iostream>
 #include <vector>
 #include <cassert>
 
 using namespace std;
 
-// 回溯法
-// 时间复杂度: O(m*n*m*n)
-// 空间复杂度: O(m*n)
+/// 79. Word Search
+/// Source : https://leetcode.com/problems/word-search/description/
+///
+/// 回溯法
+/// 时间复杂度: O(m*n*m*n)
+/// 空间复杂度: O(m*n)
 class Solution {
 
 private:
@@ -18,26 +17,26 @@ private:
     int m, n;
     vector<vector<bool>> visited;
 
-    bool inArea( int x , int y ){
+    bool inArea(int x, int y){
         return x >= 0 && x < m && y >= 0 && y < n;
     }
 
     // 从board[startx][starty]开始, 寻找word[index...word.size())
-    bool searchWord( const vector<vector<char>> &board, const string& word, int index,
+    bool searchWord(const vector<vector<char>> &board, const string& word, int index,
                     int startx, int starty ){
 
-        //assert( inArea(startx,starty) );
-        if( index == word.size() - 1 )
+        //assert(inArea(startx,starty));
+        if(index == word.size() - 1)
             return board[startx][starty] == word[index];
 
-        if( board[startx][starty] == word[index] ){
+        if(board[startx][starty] == word[index]){
             visited[startx][starty] = true;
             // 从startx, starty出发,向四个方向寻
-            for( int i = 0 ; i < 4 ; i ++ ){
+            for(int i = 0 ; i < 4 ; i ++){
                 int newx = startx + d[i][0];
                 int newy = starty + d[i][1];
-                if( inArea(newx, newy) && !visited[newx][newy] &&
-                    searchWord(board, word, index + 1, newx, newy))
+                if(inArea(newx, newy) && !visited[newx][newy] &&
+                   searchWord(board, word, index + 1, newx, newy))
                     return true;
             }
             visited[startx][starty] = false;
@@ -49,12 +48,17 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
 
         m = board.size();
-        assert( m > 0 );
+        assert(m > 0);
         n = board[0].size();
-        visited = vector<vector<bool>>(m, vector<bool>(n, false));
-        for( int i = 0 ; i < board.size() ; i ++ )
-            for( int j = 0 ; j < board[i].size() ; j ++ )
-                if( searchWord( board, word, 0 , i, j) )
+        assert(n > 0);
+
+        visited.clear();
+        for(int i = 0 ; i < m ; i ++)
+            visited.push_back(vector<bool>(n, false));
+
+        for(int i = 0 ; i < board.size() ; i ++)
+            for(int j = 0 ; j < board[i].size() ; j ++)
+                if(searchWord(board, word, 0, i, j))
                     return true;
 
         return false;
@@ -63,30 +67,32 @@ public:
 
 int main() {
 
-    char b1[3][4] = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+    char b1[3][4] = {{'A', 'B', 'C', 'E'},
+                     {'S', 'F', 'C', 'S'},
+                     {'A', 'D', 'E', 'E'}};
     vector<vector<char>> board1;
     for( int i = 0 ; i < 3 ; i ++ )
         board1.push_back(vector<char>(b1[i], b1[i] + sizeof(b1[i]) / sizeof(char)));
 
     int cases = 3;
-    string words[3] = {"ABCCED" , "SEE" , "ABCB" };
-    for( int i = 0 ; i < cases ; i ++ )
+    string words[3] = {"ABCCED", "SEE", "ABCB" };
+    for(int i = 0 ; i < cases ; i ++)
         if(Solution().exist(board1,words[i]))
-            cout<<"found "<<words[i]<<endl;
+            cout << "found " << words[i] << endl;
         else
-            cout<<"can not found "<<words[i]<<endl;
+            cout << "can not found " << words[i] << endl;
 
     // ---
 
     char b2[1][1] = {{'A'}};
     vector<vector<char>> board2;
     for(int i = 0 ; i < 3 ; i ++)
-        board2.push_back(vector<char>(b2[i],b2[i]+sizeof(b2[i])/sizeof(char)));
+        board2.push_back(vector<char>(b2[i], b2[i] + sizeof(b2[i])/sizeof(char)));
 
     if(Solution().exist(board2,"AB"))
-        cout<<"found AB"<<endl;
+        cout << "found AB" << endl;
     else
-        cout<<"can not found AB"<<endl;
+        cout << "can not found AB" << endl;
 
     return 0;
 }
--- 08-Recurion-and-Backstracking/Course Code (Java)/06-Word-Search/src/Solution.java
+++ 08-Recurion-and-Backstracking/Course Code (Java)/06-Word-Search/src/Solution.java
@@ -1,10 +1,13 @@
-// 回溯法
-// 时间复杂度: O(m*n*m*n)
-// 空间复杂度: O(m*n)
+/// 79. Word Search
+/// Source : https://leetcode.com/problems/word-search/description/
+///
+/// 回溯法
+/// 时间复杂度: O(m*n*m*n)
+/// 空间复杂度: O(m*n)
 public class Solution {
 
-    private int d[][] = {{-1,0},{0,1},{1,0},{0,-1}};
-    private int m,n;
+    private int d[][] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    private int m, n;
     private boolean[][] visited;
 
     public boolean exist(char[][] board, String word) {
@@ -13,13 +16,16 @@ public class Solution {
             throw new IllegalArgumentException("board or word can not be null!");
 
         m = board.length;
-        assert( m > 0 );
+        if(m == 0)
+            throw new IllegalArgumentException("board can not be empty.");
         n = board[0].length;
+        if(n == 0)
+            throw new IllegalArgumentException("board can not be empty.");
 
         visited = new boolean[m][n];
-        for( int i = 0 ; i < m ; i ++ )
-            for( int j = 0 ; j < n ; j ++ )
-                if( searchWord( board, word, 0 , i, j) )
+        for(int i = 0 ; i < m ; i ++)
+            for(int j = 0 ; j < n ; j ++)
+                if(searchWord(board, word, 0, i, j))
                     return true;
 
         return false;
@@ -30,21 +36,21 @@ public class Solution {
     }
 
     // 从board[startx][starty]开始, 寻找word[index...word.size())
-    private boolean searchWord( char[][] board, String word, int index,
-                     int startx, int starty ){
+    private boolean searchWord(char[][] board, String word, int index,
+                               int startx, int starty){
 
-        //assert( inArea(startx,starty) );
-        if( index == word.length() - 1 )
+        //assert(inArea(startx,starty));
+        if(index == word.length() - 1)
             return board[startx][starty] == word.charAt(index);
 
-        if( board[startx][starty] == word.charAt(index) ){
+        if(board[startx][starty] == word.charAt(index)){
             visited[startx][starty] = true;
             // 从startx, starty出发,向四个方向寻
-            for( int i = 0 ; i < 4 ; i ++ ){
+            for(int i = 0 ; i < 4 ; i ++){
                 int newx = startx + d[i][0];
                 int newy = starty + d[i][1];
-                if( inArea(newx, newy) && !visited[newx][newy] &&
-                        searchWord( board , word , index + 1 , newx , newy ) )
+                if(inArea(newx, newy) && !visited[newx][newy] &&
+                        searchWord(board, word, index + 1, newx, newy))
                     return true;
             }
             visited[startx][starty] = false;
@@ -54,11 +60,13 @@ public class Solution {
 
     public static void main(String args[]){
 
-        char[][] b1 = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        char[][] b1 = { {'A','B','C','E'},
+                        {'S','F','C','S'},
+                        {'A','D','E','E'}};
 
-        String words[] = {"ABCCED" , "SEE" , "ABCB" };
-        for( int i = 0 ; i < words.length ; i ++ )
-            if( (new Solution()).exist(b1, words[i]))
+        String words[] = {"ABCCED", "SEE", "ABCB" };
+        for(int i = 0 ; i < words.length ; i ++)
+            if((new Solution()).exist(b1, words[i]))
                 System.out.println("found " + words[i]);
             else
                 System.out.println("can not found " + words[i]);
@@ -66,7 +74,7 @@ public class Solution {
         // ---
 
         char[][] b2 = {{'A'}};
-        if( (new Solution()).exist(b2,"AB"))
+        if((new Solution()).exist(b2,"AB"))
             System.out.println("found AB");
         else
             System.out.println("can not found AB");
