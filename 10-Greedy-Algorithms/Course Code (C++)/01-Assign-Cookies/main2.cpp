d1cb77fc2d1f60daa2b96d1a0d23a190cfd2e5c1
liuyubobobo <liuyubobobo@gmail.com>
2018-01-17 22:26:27 -0800

    Chapter 10 section 01 new algorithm added.

--- 10-Greedy-Algorithms/Course Code (C++)/01-Assign-Cookies/main2.cpp
+++ 10-Greedy-Algorithms/Course Code (C++)/01-Assign-Cookies/main2.cpp
@@ -6,15 +6,15 @@ using namespace std;
 
 /// 455. Assign Cookies
 /// https://leetcode.com/problems/assign-cookies/description/
-/// 先尝试满足最贪心的小朋友
+/// 先尝试满足最不贪心的小朋友
 /// 时间复杂度: O(nlogn)
 /// 空间复杂度: O(1)
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
 
-        sort(g.begin(), g.end(), greater<int>());
-        sort(s.begin(), s.end(), greater<int>());
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
 
         int gi = 0, si = 0;
         int res = 0;
@@ -25,7 +25,7 @@ public:
                 gi ++;
             }
             else
-                gi ++;
+                si ++;
         }
 
         return res;
--- 10-Greedy-Algorithms/Course Code (Java)/01-Assign-Cookies/src/Solution2.java
+++ 10-Greedy-Algorithms/Course Code (Java)/01-Assign-Cookies/src/Solution2.java
@@ -2,24 +2,24 @@ import java.util.Arrays;
 
 /// 455. Assign Cookies
 /// https://leetcode.com/problems/assign-cookies/description/
-/// 先尝试满足最贪心的小朋友
+/// 先尝试满足最不贪心的小朋友
 /// 时间复杂度: O(nlogn)
 /// 空间复杂度: O(1)
-public class Solution {
+public class Solution2 {
 
     public int findContentChildren(int[] g, int[] s) {
 
         Arrays.sort(g);
         Arrays.sort(s);
 
-        int gi = g.length - 1, si = s.length - 1;
+        int gi = 0, si = 0;
         int res = 0;
-        while(gi >= 0 && si >= 0){
+        while(gi < g.length && si < s.length){
             if(s[si] >= g[gi]){
                 res ++;
-                si --;
+                gi ++;
             }
-            gi --;
+            si ++;
         }
 
         return res;
@@ -29,10 +29,10 @@ public class Solution {
 
         int g1[] = {1, 2, 3};
         int s1[] = {1, 1};
-        System.out.println((new Solution()).findContentChildren(g1, s1));
+        System.out.println((new Solution2()).findContentChildren(g1, s1));
 
         int g2[] = {1, 2};
         int s2[] = {1, 2, 3};
-        System.out.println((new Solution()).findContentChildren(g2, s2));
+        System.out.println((new Solution2()).findContentChildren(g2, s2));
     }
 }
